d8098f24509865362e48c644504f1073d70aeddb
quyetthang <thang13513110061@gmail.com>
2020-03-26 09:27:50 +0700

    - Fixed MethodChannel flutter_lantern

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Program Files\SDK-Fluter\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\ProjectOthers\Flutter\flutter_flash\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Program Files\SDK-Fluter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
--- lib/flutter_lantern.dart
+++ lib/flutter_lantern.dart
@@ -3,11 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Lantern {
-  static const MethodChannel _channel = const MethodChannel('github.com/elratonmaton/flutter_lantern');
-  /*static Future<bool> get hasLamp async {
-    final bool haslamp = await _channel.invokeMethod('hasLamp');
-    return haslamp;
-  }*/
+  static const MethodChannel _channel = const MethodChannel('flutter_lantern');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
@@ -19,7 +15,11 @@ class Lantern {
     return haslamp;
   }
 
-  static Future turnOn({double intensity = 1.0}) => _channel.invokeMethod('turnOn', {'intensity' : intensity.clamp(0.01, 1.0)});
+  static Future turnOn({double intensity = 1.0}) => _channel
+      .invokeMethod('turnOn', {'intensity': intensity.clamp(0.01, 1.0)});
+
   static Future turnOff() => _channel.invokeMethod('turnOff');
-  static Future flash(Duration duration) => turnOn().whenComplete(() => new Future.delayed(duration, () => turnOff()));
+
+  static Future flash(Duration duration) => turnOn()
+      .whenComplete(() => new Future.delayed(duration, () => turnOff()));
 }
